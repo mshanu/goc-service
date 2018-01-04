7d68bad8e4752d98019968ba3a4ceda4b0fb4397
Shanu M <shanu@IN-CH-Shanu.local>
2018-01-04 09:30:18 +0530

    Shanu: Updating the challenges

--- challenges/challenges.js
+++ challenges/challenges.js
@@ -1,14 +1,10 @@
 const mongoose = require('./../src/models/mongoose');
 const Challenge = require('./../src/models/challenge');
 const fs = require('fs');
-
+//
 // Challenge.remove({}).then(
 //     Promise.all([
-//         new Challenge(JSON.parse(fs.readFileSync("/Users/shanu/Projects/goc/service/challenges/0.json"))).save(),
-//         new Challenge(JSON.parse(fs.readFileSync("/Users/shanu/Projects/goc/service/challenges/1.json"))).save(),
-//         new Challenge(JSON.parse(fs.readFileSync("/Users/shanu/Projects/goc/service/challenges/2.json"))).save(),
-//         new Challenge(JSON.parse(fs.readFileSync("/Users/shanu/Projects/goc/service/challenges/3.json"))).save(),
-//         new Challenge(JSON.parse(fs.readFileSync("/Users/shanu/Projects/goc/service/challenges/4.json"))).save()
+//         new Challenge(JSON.parse(fs.readFileSync("/Users/shanu/Projects/goc/service/challenges/august/0.json"))).save()
 //     ])).then(() => console.log("Finished " + arguments));
 
 Challenge.remove({}).then(
